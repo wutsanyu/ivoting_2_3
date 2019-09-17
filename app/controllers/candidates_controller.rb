119d50f283d37c73a57059f74425a3f762b6c778
wutsanyu <b97101053@gmail.com>
2019-09-17 13:36:42 +0800

    MVC建立完成

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,44 @@
+class CandidatesController < ApplicationController
+
+  def index
+    @candidates = Candidate.all
+  end
+
+  def new
+    @candidate = Candidate.new
+  end
+
+  def create
+    @candidate = Candidate.new(candidate_params)
+    if @candidate.save
+      redirect_to candidates_path, notice: "新增候選人成功"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @candidate = Candidate.find_by(id: params[:id])
+  end
+
+  def update
+    @candidate = Candidate.find_by(id: params[:id])
+
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path, notice: "更新資料成功"
+    else
+      render :edit
+    end
+  end 
+
+  def destroy
+    @candidate = Candidate.find_by(id: params[:id])
+    @candidate.destroy if @candidate
+    redirect_to candidates_path, notice: "候選人資料已刪除"
+  end
+
+  private
+  def candidate_params
+    params.require(:candidate).permit(:name, :age, :party, :politics)
+  end
+end
